8835c4604d4aed131e4fa25e04a23af87a27b09c
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-08-18 18:10:10 -0400

    update

--- examples/main.py
+++ examples/main.py
@@ -20,5 +20,6 @@ atexit.register(turnOffMotors)
 myStepper = mh.getStepper(200, 1)  # 200 steps/rev, motor port #1
 myStepper.setSpeed(100)             # 30 RPM
 
-myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.SINGLE)
-myStepper.step(100, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
+myStepper.step(200, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.SINGLE)
+time.sleep(1)
+myStepper.step(200, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
