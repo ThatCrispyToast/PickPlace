1932398b412c833d60c3fc96494d0ee2eb0f0dfe
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-08-18 19:43:58 -0400

    update

--- examples/main.py
+++ examples/main.py
@@ -4,9 +4,10 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_Step
 
 import time
 import atexit
+import threading
 
 X_STEPS = 1490
-Y_STEPS = None
+Y_STEPS = 1580
 
 # create a default object, no changes to I2C address or frequency
 mh = Adafruit_MotorHAT()
@@ -17,22 +18,30 @@ def turnOffMotors():
     mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+atexit.register(turnOffMotors)
+
+def hola():
+    print("hola")
+
+def hello():
+    print("hello")
 
 def main():
-    atexit.register(turnOffMotors)
-
-    myStepper = mh.getStepper(200, 2)  # 200 steps/rev, motor port #1
+    myStepper = mh.getStepper(200, 1)  # 200 steps/rev, motor port #1
     myStepper.setSpeed(3000)             # 30 RPM
+    myStepper2 = mh.getStepper(200, 2)  # 200 steps/rev, motor port #1
+    myStepper2.setSpeed(3000)             # 30 RPM
 
-    start = time.time()
-    myStepper.step(1579, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.SINGLE)
-    print(time.time() - start)
+    # create empty threads (these will hold the stepper 1 and 2 threads)
+    st1 = threading.Thread(target=hola)
+    st2 = threading.Thread(target=hello)
 
-    # c = 0
-    # while True:
-    #     myStepper.step(50, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.SINGLE)
-    #     c += 1
-    #     print(c, " 1/4 Rotations")
+    st1.start()
+    st2.start()
+
+    st1.join()
+    st2.join()
+
 
 if __name__ == "__main__":
     main()
