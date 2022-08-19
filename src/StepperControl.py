8ba9120e8afbfaa94ee897bdf8172bc2665a3ae4
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-08-19 19:29:50 -0400

    Automatic Sync

--- src/StepperControl.py
+++ src/StepperControl.py
@@ -54,7 +54,6 @@ class StepperControl:
         self.xbusy = True
         x_stepper.step(steps, direction,  step_type)
         self.x_steps += steps
-        print(self.x_steps)
         self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
         self.xbusy = False
         return True
--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@ from StepperControl import StepperControl
 import time
 
 steppers = StepperControl()
-print("asd")
 
 # start = time.time()
 # steppers.move_y(523, StepperControl.FORWARD, StepperControl.SINGLE, block=False)
 # steppers.move_x(StepperControl.X_LENGTH/2, StepperControl.LEFT, StepperControl.SINGLE, block=True)
 # print(time.time() - start)
 
-print(steppers.move_xy(StepperControl.X_LENGTH/2, StepperControl.LEFT, StepperControl.SINGLE, 523, StepperControl.FORWARD, StepperControl.SINGLE))
+print(steppers.move_xy(StepperControl.X_LENGTH/2, StepperControl.LEFT, StepperControl.SINGLE,
+                        523, StepperControl.FORWARD, StepperControl.SINGLE))
