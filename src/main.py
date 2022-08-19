a88922e70a42fde99cfee23b009d97b8620266c1
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-08-18 20:10:26 -0400

    update

--- src/main.py
+++ src/main.py
@@ -21,11 +21,11 @@ def turnOffMotors():
 atexit.register(turnOffMotors)
 
 def x_control(myStepper):
-    myStepper.step(X_STEPS/4, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.DOUBLE)
+    myStepper.step(X_STEPS/4, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.INTERLEAVE)
     print("X Done")
 
 def y_control(myStepper2):
-    myStepper2.step(Y_STEPS/4, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.DOUBLE)
+    myStepper2.step(Y_STEPS/4, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.INTERLEAVE)
     print("Y Done")
 
 def main():
