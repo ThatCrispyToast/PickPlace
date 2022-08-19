e2d1bcf2010ea8148dce6f4470deec9bf8a5f015
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-08-18 20:00:04 -0400

    update

--- src/main.py
+++ src/main.py
@@ -36,11 +36,16 @@ def main():
     st1 = threading.Thread(target=x_control, args=(myStepper,))
     st2 = threading.Thread(target=y_control, args=(myStepper2,))
 
+    st1.setDaemon(True)
+    st2.setDaemon(True)
+
     st1.start()
     st2.start()
 
     st1.join()
     st2.join()
+
+    print("Hello")
 
 
 if __name__ == "__main__":
