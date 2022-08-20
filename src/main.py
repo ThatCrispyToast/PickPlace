3f2f2687d973b4705478326b3968f369b8166ebc
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-08-19 21:52:36 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import time
 steppers = StepperControl()
 
 # Moves X Rail to 1/4 of It's Rail Length to the Left 
-steppers.move_x(StepperControl.X_LENGTH/8, StepperControl.LEFT, StepperControl.SINGLE)
+steppers.move_x(StepperControl.X_LENGTH/4, StepperControl.LEFT, StepperControl.SINGLE, block=False)
+time.sleep(1)
 print(steppers.get_position())
 print("Hello!")
