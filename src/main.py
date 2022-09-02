9adbdeb81e916f628015590b16500599895d7792
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-02 17:37:35 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -3,3 +3,4 @@ from stepper_control import StepperControl
 stepper_control = StepperControl()
 
 stepper_control.move_x(StepperControl.X_LENGTH, StepperControl.LEFT)
+print()
--- src/stepper_control.py
+++ src/stepper_control.py
@@ -21,6 +21,9 @@ class StepperControl:
         self.kit = MotorKit()
         self.kit2 = MotorKit(address=0x61)
         atexit.register(self.release)
+        self.x_pos = 0
+        self.y_pos = 0
+        self.z_pos = 0
 
     def release(self):
         self.kit.stepper1.release()
@@ -31,15 +34,21 @@ class StepperControl:
     def move_x(self, steps, direction=LEFT):
         for _ in range(steps):
             self.kit.stepper1.onestep(direction=direction, style=stepper.DOUBLE)
+            self.x_pos += 1 if direction == StepperControl.LEFT else -1
         self.release()
     
     def move_y(self, steps, direction=FORWARD):
         for _ in range(steps):
             self.kit.stepper2.onestep(direction=direction, style=stepper.DOUBLE)
+            self.y_pos += 1 if direction == StepperControl.FORWARD else -1
         self.release()
 
     def move_z(self, steps, direction=UP):
         for _ in range(steps):
             self.kit2.stepper1.onestep(direction=direction, style=stepper.DOUBLE)
             self.kit2.stepper2.onestep(direction=direction, style=stepper.DOUBLE)
+            self.z_pos += 1 if direction == StepperControl.UP else -1
         self.release()
+    
+    def get_pos(self):
+        return (self.x_pos, self.y_pos, self.z_pos)
