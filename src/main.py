8b2e05978cd0c293de37429cfc14a18c0b2b779a
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-02 17:38:57 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ from stepper_control import StepperControl
 
 stepper_control = StepperControl()
 
-stepper_control.move_x(StepperControl.X_LENGTH/2, StepperControl.LEFT)
+stepper_control.move_x(int(StepperControl.X_LENGTH/2), StepperControl.LEFT)
 print(stepper_control.get_pos())
