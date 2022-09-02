1ca3c3b16216153a843fbd85bbc680fcd87e1c44
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-02 16:19:33 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -16,8 +16,8 @@ for i in range(400):
     kit2.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
 print("single")
 for i in range(400):
-    kit2.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-    kit2.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+    kit2.stepper1.onestep(direction=stepper.BACKWARD)
+    kit2.stepper2.onestep(direction=stepper.BACKWARD)
 
 
 release()
