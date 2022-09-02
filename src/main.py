6c35e679f0dd5d5fe87a2bbc195e884e64891675
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-02 17:59:27 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -3,27 +3,6 @@ import time
 
 steppers = StepperControl()
 
-start = time.time()
-steppers.move_z(steppers.Z_LENGTH, StepperControl.UP)
-upz = time.time() - start
-print(upz)
-print(steppers.get_pos())
-start = time.time()
-steppers.move_z(steppers.Z_LENGTH, StepperControl.DOWN)
-downz = time.time() - start
-print(downz)
-print(steppers.get_pos())
-
-start = time.time()
-steppers.move_x(steppers.X_LENGTH, StepperControl.LEFT)
-leftx = time.time() - start
-print(leftx)
-print(steppers.get_pos())
-start = time.time()
-steppers.move_z(steppers.X_LENGTH, StepperControl.RIGHT)
-rightx = time.time() - start
-print(rightx)
-print(steppers.get_pos())
 
 start = time.time()
 steppers.move_y(steppers.Y_LENGTH, StepperControl.FORWARD)
@@ -35,5 +14,3 @@ steppers.move_y(steppers.Y_LENGTH, StepperControl.BACKWARD)
 backwardy = time.time() - start
 print(backwardy)
 print(steppers.get_pos())
-
-print(upz, downz, leftx, rightx, forwardy, backwardy)
