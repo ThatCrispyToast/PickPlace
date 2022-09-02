a8e19ee3f357c5f449dc1b25797c7c9f875573b8
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-02 17:55:28 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -5,9 +5,35 @@ steppers = StepperControl()
 
 start = time.time()
 steppers.move_z(steppers.Z_LENGTH, StepperControl.UP)
-print(time.time() - start)
+upz = time.time() - start
+print(upz)
 print(steppers.get_pos())
 start = time.time()
 steppers.move_z(steppers.Z_LENGTH, StepperControl.DOWN)
-print(time.time() - start)
+downz = time.time() - start
+print(downz)
 print(steppers.get_pos())
+
+start = time.time()
+steppers.move_x(steppers.X_LENGTH, StepperControl.LEFT)
+leftx = time.time() - start
+print(leftx)
+print(steppers.get_pos())
+start = time.time()
+steppers.move_z(steppers.X_LENGTH, StepperControl.RIGHT)
+rightx = time.time() - start
+print(rightx)
+print(steppers.get_pos())
+
+start = time.time()
+steppers.move_y(steppers.Y_LENGTH, StepperControl.FORWARD)
+forwardy = time.time() - start
+print(forwardy)
+print(steppers.get_pos())
+start = time.time()
+steppers.move_y(steppers.Y_LENGTH, StepperControl.BACKWARD)
+backwardy = time.time() - start
+print(backwardy)
+print(steppers.get_pos())
+
+print(upz, downz, leftx, rightx, forwardy, backwardy)
