c100e9781e9e2daf62c9f386a9f7ff01030b974b
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-02 16:08:37 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -1,23 +1,11 @@
-# from StepperControl import StepperControl
-# import time
-
-# steppers = StepperControl()
-
-# # start = time.time()
-# # steppers.move_y(523, StepperControl.FORWARD, StepperControl.SINGLE, block=False)
-# # steppers.move_x(StepperControl.X_LENGTH/2, StepperControl.LEFT, StepperControl.SINGLE, block=True)
-# # print(time.time() - start)
-
-# print(steppers.move_xy(StepperControl.X_LENGTH/2, StepperControl.LEFT, StepperControl.SINGLE,
-#                         523, StepperControl.FORWARD, StepperControl.SINGLE))
-
-
-#!/usr/bin/python
-#import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_Stepper
 import time
 from adafruit_motorkit import MotorKit
 
 kit = MotorKit()
 
-for i in range(1490):
+print(kit.motor1.throttle)
+
+for i in range(200):
     kit.stepper1.onestep()
+# for i in range(100):
+#     kit.stepper1.onestep()
