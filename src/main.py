76215361bd28436bfb665587acaa5df57c5a3865
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-02 17:23:52 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -1,26 +1,5 @@
-import time
-from adafruit_motorkit import MotorKit
-from adafruit_motor import stepper
+from stepper_control import StepperControl
 
-kit = MotorKit()
-kit2 = MotorKit(address=0x61)
+stepper_control = StepperControl()
 
-def release():
-    kit.stepper1.release()
-    kit.stepper2.release()
-    kit2.stepper1.release()
-    kit2.stepper2.release()
-
-x = time.time()
-
-for i in range(1490):
-    kit.stepper1.onestep(style=stepper.DOUBLE)
-
-for i in range(1490):
-    kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-    # kit2.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-
-print(time.time() - x)
-
-
-release()
+stepper_control.move_x(100)
---
+++ src/stepper_control.py
@@ -0,0 +1,19 @@
+from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper
+from main import release
+
+class StepperControl:
+    def __init__(self):
+        self.kit = MotorKit()
+        self.kit2 = MotorKit(address=0x61)
+
+    def release(self):
+        self.kit.stepper1.release()
+        self.kit.stepper2.release()
+        self.kit2.stepper1.release()
+        self.kit2.stepper2.release()
+
+    def move_x(self, steps):
+        for _ in range(steps):
+            self.kit.stepper1.onestep(style=stepper.DOUBLE)
+        self.release()
