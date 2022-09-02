7227071725ff705cf3dcef2329507bf588cebbab
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-02 17:49:25 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from stepper_control import StepperControl
+import time
 
 steppers = StepperControl()
 
+start = time.time()
 steppers.move_z(steppers.Z_LENGTH, StepperControl.UP)
+print(time.time() - start)
 print(steppers.get_pos())
+start = time.time()
 steppers.move_z(steppers.Z_LENGTH, StepperControl.DOWN)
+print(time.time() - start)
 print(steppers.get_pos())
