a00f4993d3b81745190002a2bdc17a1a3c1987a0
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-02 17:28:59 -0400

    Automatic Sync

--- src/stepper_control.py
+++ src/stepper_control.py
@@ -2,6 +2,14 @@ from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
 
 class StepperControl:
+
+    UP = stepper.BACKWARD
+    DOWN = stepper.FORWARD
+    RIGHT = stepper.BACKWARD
+    LEFT = stepper.FORWARD
+    BACKWARD = stepper.BACKWARD
+    FORWARD = stepper.FORWARD
+
     def __init__(self):
         self.kit = MotorKit()
         self.kit2 = MotorKit(address=0x61)
@@ -12,7 +20,18 @@ class StepperControl:
         self.kit2.stepper1.release()
         self.kit2.stepper2.release()
 
-    def move_x(self, steps):
+    def move_x(self, steps, direction=LEFT):
         for _ in range(steps):
-            self.kit.stepper1.onestep(style=stepper.DOUBLE)
+            self.kit.stepper1.onestep(direction=direction, style=stepper.DOUBLE)
+        self.release()
+    
+    def move_y(self, steps, direction=FORWARD):
+        for _ in range(steps):
+            self.kit.stepper2.onestep(direction=direction, style=stepper.DOUBLE)
+        self.release()
+
+    def move_z(self, steps, direction=UP):
+        for _ in range(steps):
+            self.kit2.stepper1.onestep(direction=direction, style=stepper.DOUBLE)
+            self.kit2.stepper2.onestep(direction=direction, style=stepper.DOUBLE)
         self.release()
