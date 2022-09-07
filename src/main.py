77e2b8b337ea608ec19bf8e8388f7c1700dd8396
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-07 16:38:50 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -2,4 +2,4 @@ from stepper_control import StepperControl
 
 steppers = StepperControl()
 
-steppers.move_asyc(int(steppers.X_LENGTH/2), int(steppers.Y_LENGTH/4), int(steppers.Z_LENGTH/16))
+steppers.move_async(int(steppers.X_LENGTH/2), int(steppers.Y_LENGTH/4), int(steppers.Z_LENGTH/16))
