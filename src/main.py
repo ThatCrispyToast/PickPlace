402514b479384d53842f85f78c996bcc6587e408
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-07 16:45:00 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -5,9 +5,12 @@ steppers = StepperControl()
 
 total = 0
 
-for _ in range(10):
+for _ in range(3):
     start = time.time()
     steppers.move(int(steppers.X_LENGTH/2), int(steppers.Y_LENGTH/4), int(steppers.Z_LENGTH/16))
-    total = time.time() - start
+    total += time.time() - start
+    start = time.time()
+    steppers.move(int(-steppers.X_LENGTH/2), int(-steppers.Y_LENGTH/4), int(-steppers.Z_LENGTH/16))
+    total += time.time() - start
 
 print(total/10)
