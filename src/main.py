76a563524dd02bb9c7ba60ab3d3ead1bf8daf6c0
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-07 16:42:20 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from stepper_control import StepperControl
+import time
 
 steppers = StepperControl()
 
 steppers.move_async(int(steppers.X_LENGTH/2), int(steppers.Y_LENGTH/4), int(steppers.Z_LENGTH/16))
+time.sleep(10)
+
+print("wasd")
