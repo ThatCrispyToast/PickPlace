2177119030a80003728c6c89de203c614b9d4bd1
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-07 17:03:37 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -4,13 +4,13 @@ import time
 steppers = StepperControl()
 
 start = time.time()
-steppers.move_async(int(steppers.X_LENGTH/2), int(steppers.Y_LENGTH/4), int(steppers.Z_LENGTH/32))
+steppers.move_async(int(-steppers.X_LENGTH/2), int(-steppers.Y_LENGTH/4), int(-steppers.Z_LENGTH/32))
 while steppers.is_running():
     time.sleep(0.1)
 print(time.time() - start)
 
 start = time.time()
-steppers.move(int(steppers.X_LENGTH/2), int(steppers.Y_LENGTH/4), int(steppers.Z_LENGTH/32))
+steppers.move(int(-steppers.X_LENGTH/2), int(-steppers.Y_LENGTH/4), int(-steppers.Z_LENGTH/32))
 print(time.time() - start)
 
 print("Done")
