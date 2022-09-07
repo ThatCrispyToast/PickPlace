4700d3a385aad5250fd294370369baddab2789d9
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-07 16:48:01 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -8,9 +8,13 @@ total = 0
 for _ in range(3):
     start = time.time()
     steppers.move_async(int(steppers.X_LENGTH/2), int(steppers.Y_LENGTH/4), int(steppers.Z_LENGTH/16))
+    while steppers.is_running():
+        pass
     total += time.time() - start
     start = time.time()
     steppers.move_async(int(-steppers.X_LENGTH/2), int(-steppers.Y_LENGTH/4), int(-steppers.Z_LENGTH/16))
+    while steppers.is_running():
+        pass
     total += time.time() - start
 
 print(total/3)
