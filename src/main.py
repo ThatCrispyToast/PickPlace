5eb8a278b7a3e587685f9ca061148e4748a6ec92
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-09-23 16:00:43 -0400

    Automatic Sync

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import time
 steppers = StepperControl()
 
 start = time.time()
-steppers.move(-steppers.X_LENGTH/4, 0, steppers.Z_LENGTH/16)
+steppers.move(0, 0, steppers.Z_LENGTH/16)
 print(time.time() - start)
 
 print("Done")
